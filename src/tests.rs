d885bad8827543977c5b16a0c22155d0c22bb724
Patrick Elsen <pelsen@xfbs.net>
2017-07-28 20:47:27 +0200

    removes unused tests

--- src/tests.rs
+++ src/tests.rs
@@ -2,72 +2,16 @@ extern crate regex;
 use self::regex::Regex;
 use super::*;
 
-/*
-#[test]
-fn parse_args_help_works() {
-    assert_eq!(parse_args(vec!["-h".to_string()]), Ok(ShowHelp));
-    assert_eq!(parse_args(vec!["--help".to_string()]), Ok(ShowHelp));
-}
-
-#[test]
-fn parse_args_version_works() {
-    assert_eq!(parse_args(vec!["-v".to_string()]), Ok(ShowVersion));
-    assert_eq!(parse_args(vec!["--version".to_string()]), Ok(ShowVersion));
-}
-
-#[test]
-fn parse_args_uses_default_string() {
-    assert_eq!(parse_args(vec![]), Ok(RepeatString(DEFAULT_STRING.to_string())));
-}
-
-#[test]
-fn parse_args_uses_arguments() {
-    assert_eq!(parse_args(vec!["str".to_string()]),
-               Ok(RepeatString("str".to_string())));
-}
-
-#[test]
-fn parse_args_concatenates_arguments() {
-    assert_eq!(parse_args(vec!["str".to_string(), "dup".to_string()]),
-               Ok(RepeatString("str dup".to_string())));
-}
-
-#[test]
-fn parse_args_allows_escaped_flags() {
-    assert_eq!(parse_args(vec!["--".to_string(), "-h".to_string()]),
-               Ok(RepeatString("-h".to_string())));
-}
-
-#[test]
-fn parse_args_raises_error_on_illegal_argument() {
-    assert!(parse_args(vec!["-g".to_string()]).is_err());
-    match parse_args(vec!["--global".to_string()]) {
-        Err(InvalidOption(_)) => assert!(true),
-        _ => unreachable!()
-    }
-}
-
-#[test]
-fn parse_args_error_has_message() {
-    let re = Regex::new(r"Unrecognized option").unwrap();
-
-    assert!(parse_args(vec!["-g".to_string()]).is_err());
-    match parse_args(vec!["--global".to_string()]) {
-        Err(e) => assert!(re.is_match(&e.message())),
-        _ => unreachable!()
-    }
-}
-*/
 #[test]
 fn string_newline_terminate_works() {
-    assert_eq!("word".to_string().newline_terminate(), "word\n");
-    assert_eq!("myname".to_string().newline_terminate(), "myname\n");
+    assert_eq!("word".with_newline(), "word\n");
+    assert_eq!("myname".with_newline(), "myname\n");
 }
 
 #[test]
 fn string_fill_works() {
     let max = 1500;
-    let buf = "myword\n".to_string().fill(max);
+    let buf = "myword\n".fill(max);
 
     // make sure string isn't too long
     assert!(buf.len() <= max);
@@ -95,6 +39,8 @@ impl Write for Full {
         Ok(())
     }
 }
+
+
 
 #[test]
 fn write_repeat_stops_on_error() {
