117644094c0d55644955605dedbf761e95890a73
Patrick Elsen <pelsen@xfbs.net>
2017-07-28 02:30:00 +0200

    adds comments to main function

--- src/bin/yes.rs
+++ src/bin/yes.rs
@@ -1,6 +1,7 @@
 extern crate rubigo_yes;
 
 fn main() {
+    // parse the args and run
     match rubigo_yes::parse_args(std::env::args().skip(1).collect()) {
         Ok(ref req) => req.do_it(),
         Err(ref err) => err.show()
